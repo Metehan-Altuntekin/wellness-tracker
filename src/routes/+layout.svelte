bf2c485db168b3a6173f1343ed6d5f869ecc86f2
Burak Demir <45737592+burakndmr@users.noreply.github.com>
2025-03-03 21:34:25 +0300

    chore: add custom font files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,14 @@
 	The reason for this is because we are designing the app for mobile only.
 -->
 
-<div class="sm:mockup-phone sm:shadow-2xl">
+<div class="sm:mockup-phone sm:shadow-2xl font-display">
 	<div class="sm:mockup-phone-camera"></div>
-	<div class="sm:mockup-phone-display">
-		<div class="w-full h-full bg-base-200">
+	<div class="sm:mockup-phone-display min-h-full flex flex-col ">
+		<div class="hidden sm:block w-full h-12 bg-base-300"></div>
+		<div class="w-full sm:flex-1 max-sm:h-screen bg-base-200">
 			{@render children()}
 		</div>
 	</div>
 </div>
+
+<!-- TODO - FONT değiştir  -->
